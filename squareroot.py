7e424feba6f0e59636a4faa94078cad691cebd58
Bhumi Thakor <thakor273@gmail.com>
2016-06-07 13:17:33 +0530

    Add files via upload

---
+++ squareroot.py
@@ -0,0 +1,12 @@
+#importing pakage
+import math
+
+print "enter the number u want to squreroot"
+a=int(input("a:"))
+
+#for finding square root there is defuslt function sqrt() which can be accessible by math module
+print" square_root=",math.sqrt(a)
+
+#finding square root without use of math module like:x**.5
+print  "squareroot using second method"
+print a**.5
